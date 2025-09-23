45432a7fe481eefd5592b768fb5747043e73fcc2
kaip <kaipacc999@gmail.com>
2025-09-23 11:31:06 -0300

    atualização

--- dicionarios.py
+++ dicionarios.py
@@ -1,19 +1,28 @@
 from tkinter import image_types
 
-dic = {"nome" : ["Pinoquio", "Geppeto", "Alidoru"],
-       "arma" : ["Espada", "cordas", "picareta"],
-       "indole" : ["neutra", "má", "boa"],
-       "status" : ["vivo", "morto", "desconhecido"]}
+dic = {"nome"   : ["PINOQUIO", "GEPPETO",  "ALIDORU"     ],
+       "arma"   : ["Espada"  ,  "cordas",  "picareta"    ],
+       "indole" : ["neutra"  ,  "má"    ,  "boa"         ],
+       "status" : ["vivo"    ,  "morto" ,  "desconhecido"],
+       "ergo"   : [30000     ,  12000   ,   5600         ]}
 
 #-----------------------------------------------------cria indices--------------------------------------------
-#INDICES = {dic['nome'][i] : i for i in range(len(dic['nome']))}
-INDICES = {}
-for i in range(len(dic['nome'])):
-    INDICES[dic['nome'][i]] = i
+def CriaIndices(lista):
+    # INDICES = {dic['nome'][i] : i for i in range(len(dic['nome']))}
+    INDICES = {}
+    for i in range(len(lista)):
+        INDICES[lista[i]] = i
+    return INDICES
 
+def AchaIndice(lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i
 #----------------------------------------pergunta-----------------------------------------
 print(dic["nome"])
 resp = input("sobre qual personagem você deseja saber mais?")
+resp = resp.upper()
+
 
 #-----------------------------------------print/resposta--------------------------------------------
 for i in dic:
@@ -21,3 +30,16 @@ for i in dic:
     print(dic[i][INDICES[resp]])
     # dic [i = chaves] [dentro da lista indice ve o nome e volta o valor]
     # exemplo: dic [armas] [2]
+
+def MaiorErgo(lista):
+    maior = 0
+    for i in range(len(lista)):
+        if lista[i] > maior:
+            maior = lista[i]
+
+    return maior
+
+def AchaIndice(lista, elemento):
+    for i in range(len(lista)):
+        if lista[i] == elemento:
+            return i
---
+++ exercicios.py
@@ -0,0 +1,8 @@
+dic = {"nome"   : ["PINOQUIO", "GEPPETO",  "ALIDORU"     ],
+       "arma"   : ["Espada"  ,  "cordas",  "picareta"    ],
+       "indole" : ["neutra"  ,  "má"    ,  "boa"         ],
+       "status" : ["vivo"    ,  "morto" ,  "desconhecido"],
+       "ergo"   : [30000     ,  12000   ,   5600         ]}
+
+
+
